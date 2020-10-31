176b75a5980101a1d069cbc218a4a562c8f06916
yyagoub <imjou9@gmail.com>
2020-10-31 14:18:31 +0300

    first commit

--- src/controllers/controllers.js
+++ src/controllers/controllers.js
@@ -7,12 +7,14 @@ router.use('/', require('./indexController'));
 server.use(router);
 // refer static files location - so we can access it directly
 server.use(express.static('public'));
-server.use('/css', express.static(__dirname + 'public/css'));
-server.use('/img', express.static(__dirname + 'public/img'));
-server.use('/js', express.static(__dirname + 'public/js'));
+
+server.use('/css', express.static(__dirname + '../../public/css'));
+server.use('/img', express.static(__dirname + '../../public/img'));
+server.use('/js', express.static(__dirname + '../../public/js'));
+
 // Templating Engine
-server.set('layout', '../views/layouts/layout-full-width');
-server.set('views', path.join(__dirname, '../views'));
+server.set('layout', '../../../public/views/layouts/layout-full-width');
+server.set('views', path.join(__dirname, '../../public/views/pages'));
 server.set('view engine', 'ejs');
 
 module.exports = server;
